5644309fbbc51fbbe8cbeb8284bde6b633bceffc
Bahaa <bhmedhat@gmail.com>
2024-06-24 14:31:13 +0300

    Gpa Model

---
+++ src/gpa/model/Course.java
@@ -0,0 +1,39 @@
+package gpa.model;
+
+public class Course {
+	private String courseName;
+	private double grade;
+	private int creditHours;
+	
+	public Course(String courseName, int grade, int creditHours) {
+		this.setCourseName(courseName);
+		this.setGrade(grade);
+		this.setCreditHours(creditHours);
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+
+	public int getCreditHours() {
+		return creditHours;
+	}
+
+	public void setCreditHours(int creditHours) {
+		this.creditHours = creditHours;
+	}
+	
+	
+}
---
+++ src/gpa/model/GPACalculator.java
@@ -0,0 +1,66 @@
+package gpa.model;
+
+import java.util.ArrayList;
+
+public class GPACalculator {
+	ArrayList<Course> courses;
+	
+	public GPACalculator(){
+		this.courses = new ArrayList<Course>();
+	}
+	
+	public void addCourse(Course course) {
+		courses.add(course);
+	}
+	
+	public static String gpaLetter(double gpa) {
+		String letter = "";
+		
+		if(gpa >= 0.7 && gpa <= 0.99)
+			letter = "A+";
+		else if(gpa >= 1.0 && gpa <= 1.29)
+			letter = "A";
+		else if(gpa >= 1.3 && gpa <= 1.69)
+			letter = "A-";
+		else if(gpa >= 1.7 && gpa <= 1.99)
+			letter = "B+";
+		else if(gpa >= 2.0 && gpa <= 2.29)
+			letter = "B";
+		else if(gpa >= 2.3 && gpa <= 2.69)
+			letter = "B-";
+		else if(gpa >= 2.7 && gpa <= 2.99)
+			letter = "C+";
+		else if(gpa >= 3.0 && gpa <= 3.29)
+			letter = "C";
+		else if(gpa >= 3.3 && gpa <= 3.69)
+			letter = "C-";
+		else if(gpa >= 3.7 && gpa <= 3.99)
+			letter = "D+";
+		else if(gpa >= 4.0 && gpa <= 4.99)
+			letter = "D";
+		else if(gpa >= 5.0 && gpa <= 6.00)
+			letter = "F";
+		else
+			letter = "X";
+		
+		return letter;
+	}
+	
+	public String calculateGPA() {
+		String result = "Your GPA is ";
+		double totalGrade = 0;
+		int totalCreditHours = 0;
+		double gpa = 0;
+		String gpaLetter = "";
+		
+		for (Course course : courses) {
+			totalCreditHours += course.getCreditHours();
+			totalGrade += course.getGrade()*course.getCreditHours();
+		}
+		
+		gpa = totalGrade / totalCreditHours;
+		gpaLetter = gpaLetter(gpa);
+		
+		return result + gpa + "[" + gpaLetter + "]";
+	}
+}
